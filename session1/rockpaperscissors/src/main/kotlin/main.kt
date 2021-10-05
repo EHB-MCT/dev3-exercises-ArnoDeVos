432833fcfcdc749df8591a843f3872f4843508ef
ArnoDeVos <arno.de.vos@student.ehb.be>
2021-10-05 15:19:10 +0200

    try 3

--- session1/rockpaperscissors/src/main/kotlin/main.kt
+++ session1/rockpaperscissors/src/main/kotlin/main.kt
@@ -1,6 +1,32 @@
-fun main () {
-    val randomNum = (0..4)
-    var game = arrayOf(" ", "rock","paper","scissor", "")
+fun main(){
+    val options = arrayOf("rock", "paper", "scissors")
+    val optionsPc = options.random()
+    //options[(0..2).random()]
+    println("Make your choice: rock - paper - scissor")
+    val userInput = readLine()
 
-        println(game[randomNum])
+    println("You chose $userInput")
+    println("Computer chose $optionsPc")
+
+    if(userInput== "rock"){
+        when (optionsPc){
+            "rock" -> println("Draw")
+            "paper" ->println("You lose")
+            "scissors"->println("You win")
+        }
+    } else if(userInput== "paper"){
+        when (optionsPc){
+            "paper" -> println("Draw")
+            "scissors" ->println("You lose")
+            "rock"->println("You win")
+        }
+    } else if(userInput== "scissors"){
+        when (optionsPc){
+            "scissors" -> println("Draw")
+            "rock" ->println("You lose")
+            "paper"->println("You win")
+        }
+    }else{
+        println("Invalid option")
+    }
 }
