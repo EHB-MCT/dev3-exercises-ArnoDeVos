bcd02247fceeed015c8d1c2e9dc7ffb34c5defa6
ArnoDeVos <arno.de.vos@student.ehb.be>
2021-10-05 12:46:24 +0200

    2 commit

--- session1/rockpaperscissors/src/main/kotlin/main.kt
+++ session1/rockpaperscissors/src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 fun main () {
     val randomNum = (0..4)
-    var game = arrayOf("rock", "paper", "scissor")
+    var game = arrayOf(" ", "rock","paper","scissor", "")
 
         println(game[randomNum])
 }
