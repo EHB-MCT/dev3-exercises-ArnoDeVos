a45eb90eab982859e59cc0f6e5d01d091fa9f316
ArnoDeVos <arno.de.vos@student.ehb.be>
2021-10-12 11:33:09 +0200

    Challenge 1 done

--- session2/adventuregame/src/main/kotlin/main.kt
+++ session2/adventuregame/src/main/kotlin/main.kt
@@ -3,11 +3,24 @@ fun main(){
 }
 
 fun challengeOne(){
-    println("")
-    println("")
-    val correctAnswers = setOf<String>("a","c", "c")
+    println("Try to break into Erasmus")
+    println("Chose one of the buildings")
+    val correctAnswers = setOf<String>("a","b", "c")
     val userAnswer = readLine()
     if(correctAnswers.contains(userAnswer)){
+        step1()
+    }else{
+        gameOver1()
+    }
+}
+
+fun step1(){
+    println("You are infront of the building")
+    println("Which way do you want to go")
+    println("roof, window, main door or cellars")
+    val firstStep = setOf<String>("window", "cellar")
+    val userAnswer = readLine()
+    if(firstStep.contains(userAnswer)) {
         succes()
     }else{
         gameOver()
@@ -15,9 +28,11 @@ fun challengeOne(){
 }
 
 fun succes(){
-    println("You were able to break in")
+    println("You were able to enter, congrats")
 }
-
+fun gameOver1(){
+    println("Wrong choice, security is coming!!!")
+}
 fun gameOver(){
-    println("Wrong side, security is coming!!!")
+    println("Wrong choice, security is coming!!!")
 }
