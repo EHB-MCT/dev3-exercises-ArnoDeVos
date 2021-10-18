7567fd4dc2d294fcc00597819a7833123e88561e
ArnoDeVos <arno.de.vos@student.ehb.be>
2021-10-18 23:06:52 +0200

    Done till challenge 5
    Cleaned up, reworked and made it till challenge 5

--- session2/adventuregame/src/main/kotlin/main.kt
+++ session2/adventuregame/src/main/kotlin/main.kt
@@ -21,7 +21,7 @@ fun step1(){
     val firstStep = setOf<String>("window", "cellar")
     val userAnswer = readLine()
     if(firstStep.contains(userAnswer)) {
-        challenge2()
+        challengeTwo()
     }else{
         gameOver()
     }
@@ -34,33 +34,87 @@ fun gameOver1(){
     println("Wrong building, security is coming!!!")
 }
 fun gameOver(){
-    println("Wrong choice, security is coming!!!")
-}
-fun challenge2(){
-    println("You entered the building")
-    println("What do you want to steal?")
-    println("computer, teacher or servers")
-    val correctAnswers1 = setOf<String>("computer","servers")
-    val userAnswer1 = readLine()
-    if(correctAnswers1.contains(userAnswer1)){
-        money()
-    }else{
-        gameOver2()
-    }
-}
-fun money(){
-    println("well done, now it's time to find a buyer")
-}
-fun gameOver2(){
-    println("Wrong choice, who the hell wants to pay randsom for him?!?")
+    println("Security is coming!!!")
 }
 
-/*demo
-fun challengeTwo(){
-    val userGuess = readLine()
-    val diceResult = (1..6).random()
-    // todo: check == problem for strings
-    if (diceResult >=)
-    if
-}
- */
+
+fun challengeTwo() {
+    val random = (1..6).random()
+    println(random)
+
+    print("What are the chances the cops will arrest you from 1-6?")
+
+    val answer = readLine()
+
+    if((1..3).contains(random) && answer == "low") {
+        challengeThree()
+    } else if ((4..6).contains(random) && answer == "high") {
+        challengeOne()
+    }
+        else{gameOver()
+}}
+fun challengeThree(){
+        println("There is someone coming")
+        println("what will you do")
+        println("flee - fight - hide")
+
+        val userInput = readLine()
+
+        when (userInput){
+            "flee"->{
+                println("you are outside of the building again")
+                challengeOne()
+            }
+            "fight"-> {
+                println("the teacher kicked your ass")
+                gameOver()}
+            "hide"->{
+                println("the threat is gone, To continue pick a number between 1-100")
+                challengeFour()
+            }
+        }
+    }
+    val userGuess = (1..100).random()
+    fun challengeFour(){
+        val userInput = Integer.valueOf(readLine())
+
+        if (userInput == userGuess){
+            challengeFive()
+        }else if(userInput < userGuess){
+            println("Too low")
+            challengeFour()
+        }else if(userInput > userGuess){
+            println("Too high")
+            challengeFour()
+        }
+    }
+    fun challengeFive(){
+        println("Correct, now it's time to steal something")
+        val userInput = readLine()
+
+        //first
+        if(userInput == "computers"){
+            println("You got away, time to sell it")
+        }else{
+            println("They are to heavy, try something else. Something that moves on his own")
+            val userInput2 = readLine()
+
+            //second
+            if(userInput2 == "teacher"){
+                println("You got away, time to sell him")
+            }else{
+                println("who wants to pay money for a teacher")
+                println("there must be something else valuable")
+                val userInput3 = readLine()
+
+                //Third
+                if(userInput3 == "server"){
+                    println("You got away, time to sell it")
+                }else{
+                    println("You didn't find anything valuable")
+                    gameOver()
+                }
+            }
+        }
+    }
+
